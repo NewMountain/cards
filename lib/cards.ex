eca0e874be8e3adad1312efbb4d941f26e4d02d1
Unknown <christopher.nyberg@gmail.com>
2017-04-08 14:51:00 -0700

    Implemented shuffle and contains.

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,18 +1,15 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for Cards.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello
-      "Hi there!"
-
-  """
-  def hello do
-    "Hi there!"
+  def create_deck do
+    ["Ace", "Two", "Three"]
   end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end 
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
 end
