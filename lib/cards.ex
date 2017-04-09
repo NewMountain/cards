d202059188d6ae39b822a039eaaa1a917a46d492
Unknown <christopher.nyberg@gmail.com>
2017-04-08 18:23:40 -0700

    Added deal and save deck functionality

--- lib/cards.ex
+++ lib/cards.ex
@@ -8,7 +8,7 @@ defmodule Cards do
         value <- values do
       "#{value} of #{suit}"
     end
-    
+
   end
 
   def shuffle(deck) do
@@ -17,6 +17,15 @@ defmodule Cards do
 
   def contains?(deck, card) do
     Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
   end
 
 end
